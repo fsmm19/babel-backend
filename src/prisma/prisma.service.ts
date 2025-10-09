d5e96e7221382b7d29a652804f4edcc604460655
Santiago Mendez <fsmm19@proton.me>
2025-10-08 20:03:49 -0500

    feat(prisma): make PrismaModule global and export PrismaService

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,8 +1,7 @@
-import { Global, Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 
 import { PrismaClient } from 'src/generated/prisma/client';
 
-@Global()
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
